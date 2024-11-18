eac1297e43060bb64b1f83bde9e5ee54c6ba7a7a
Dylan Foster <dylanfoster935@gmail.com>
2024-11-18 00:05:01 +0000

    Sail Wind
    Created wind zone. Created sail with cloth component. As wind zone rotates, it affects the sail cloth acceleration. Need to: make both sides of cloth render correctly, make boat move according to the wind.

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class BoatController : MonoBehaviour
 {
     public Rigidbody rb;
+    public GameObject sail;
+    public ClothWind clothWind;
 
     public float speed;
     public float rotateSpeed;
+
+
+    public WindZone windZone;
+    public Rigidbody boatRigidbody;
+    public float sailEfficiency = 1f;
+    public float maxSpeed = 10f;
 
     void Start()
     {
@@ -21,7 +29,7 @@ public class BoatController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = transform.right * -vertical * speed * Time.deltaTime;
+        /*Vector3 moveDirection = transform.right * -vertical * speed * Time.deltaTime;
         rb.MovePosition(rb.position + moveDirection);
 
         if (Input.GetKey(KeyCode.D))
@@ -46,6 +54,16 @@ public class BoatController : MonoBehaviour
                 transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
 
             }
+        }*/
+        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            sail.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            sail.transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
         }
     }
 }
